8be9fc01a6e9cbdd816fd345a648a373f33f24a7
Ivan <ib4ltazar@gmail.com>
2024-01-16 00:09:36 -0600

    add network dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    //Networking
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //logs network
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
